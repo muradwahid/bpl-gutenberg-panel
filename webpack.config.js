a482a51393705d2f860d386bc8228177c8a93337
muradwahid <muradwahid334@gmail.com>
2024-01-04 15:21:28 +0600

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); 
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: 'index.js',
+    path: __dirname + './dist',
+  },
+  module: {
+    rules: [{ test: /\.js$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.js' })],
+};
