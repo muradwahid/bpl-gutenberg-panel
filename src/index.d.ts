9d94c522156f2c1de122b9e4a941f57c39bd645b
muradwahid <muradwahid334@gmail.com>
2024-06-09 21:39:09 +0600

    declare type

--- src/index.d.ts
+++ src/index.d.ts
@@ -120,10 +120,132 @@ declare const SortableProvider: ({
 }: ISortableProviderTypes) => React.JSX.Element;
 
 
+type IAdvBackground = {
+  name?: string;
+  onChange?: (value: any) => void;
+  value?: object;
+};
+declare const AdvBackground: (props: IAdvBackground) => React.JSX.Element;
+
+type IAdvOverlay = {
+  onChange?: (value: any) => void;
+  value?: object;
+}
+declare const AdvOverlay: (props: IAdvOverlay) => React.JSX.Element;
+type IUnit = {
+  label?: string;
+  value?: string;
+}
+enum IsDisable {
+  true,
+  false
+}
+type IBBoxControl = {
+  label?: string;
+  values?:object;
+  onChange?: (value:any) => void;
+  resetValues?:object,
+  defaultValues?: object;
+  units?:IUnit[];
+  sides?: string[];
+  style?: object;
+  className?: string;
+  disableUnits?:true | false;
+}
+declare const BBoxControl: (props: IBBoxControl) => React.JSX.Element;
+
+type IBButtonGroup = {
+  options?: IUnit[];
+  label?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  borderRadius?: string;
+  height?: string;
+  paddingX?: string;
+  paddingY?: string;
+  fonSize?: string;
+  fontWeight?: number;
+  activeBg?: string;
+  activeColor?: string;
+  inactiveColor?: string;
+  hoverBg?: string;
+  hoverColor?: string;
+  style?: object;
+}
+declare const BButtonGroup: (props: IBButtonGroup) => React.JSX.Element;
+
+type IBGradient = {
+  onChange?: (value: any) => void;
+  value?: object;
+};
+declare const BGradient: (props: IBGradient) => React.JSX.Element;
 
 
+type ISvalue = {
+  hOffset:any;
+  vOffset:any;
+  blur:any;
+  spreed:any;
+  color:string;
+  isInset:any;
+};
+type IBMultiShadowControl = {
+  label?: string;
+  value?: ISvalue[];
+  onChange?: (value: any) => void;
+  defaults?: ISvalue[];
+  type?: string;
+  className?: string;
+};
+declare const BMultiShadowControl: (
+  props: IBMultiShadowControl
+) => React.JSX.Element;
 
+type IBorderControl = {
+  className?: string;
+  label?: string;
+  value?: object;
+  onChange?: (value: any) => void;
+  default?: object;
+}
+declare const BorderControl: (props: IBorderControl) => React.JSX.Element;
 
+type BUnitControl = {
+  label?: string;
+  units?: IUnit[];
+  value?: string;
+  className?: string;
+  style?: object;
+  onChange?: (value: string) => void;
+  labelPosition?: "left" | "right" | "top" | "bottom";
+}
+declare const BUnitControl: (props: BUnitControl) => React.JSX.Element;
+
+type ICustomCodeEditor = {
+  value?: string;
+  onChange?: (value: string) => void;
+  height?: string;
+  width?: string;
+}
+declare const CustomCodeEditor: (props: ICustomCodeEditor) => React.JSX.Element;
+
+type ILabel = {
+  className?: string;
+  htmlFor?: string;
+}
+declare const Label: (props: ILabel) => React.JSX.Element;
+
+type IRangeUnitControl = {
+  label?: string;
+  units?: IUnit[];
+  className?: string;
+  style?: object;
+  value?: string;
+  min?: number;
+  max?: number;
+  onChange?: (value: string) => void;
+}
+declare const RangeUnitControl: (props:IRangeUnitControl)=> React.JSX.Element;
 
 export {
   Device,
@@ -138,5 +260,16 @@ export {
   PanelSingleShadow,
   Tab,
   TabPanel,
-  SortableProvider
+  SortableProvider,
+  AdvBackground,
+  AdvOverlay,
+  BBoxControl,
+  BButtonGroup,
+  BGradient,
+  BMultiShadowControl,
+  BorderControl,
+  BUnitControl,
+  CustomCodeEditor,
+  Label,
+  RangeUnitControl,
 };
