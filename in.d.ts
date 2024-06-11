be9cb6aaa51510bcaac94bbcdd17629195601038
muradwahid <muradwahid334@gmail.com>
2024-06-11 17:23:29 +0600

    remove d.ts file

--- in.d.ts
+++
@@ -1,6 +0,0 @@
-declare const getBoxCss: (value: any, property: string) => string;
-declare const getGradientCss: (gradient: any) => string;
-declare const getBackgroundCSS: (background: any) => string;
-declare const getOverlayCSS: (overlay: any) => string;
-declare const getBorderCSS: (border: any) => string;
-declare const getShadowCSS: (shadow: any) => string;
